de304694658001d77288dadea2fd57073b1998ba
Ahmed7Alwakeel <alwakeel1996@gmail.com>
2024-08-14 02:14:28 +0300

    add middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from "express"
 import "dotenv/config"
+import rateLimit from "express-rate-limit"
+import helmet from "helmet"
+import cors from "cors"
+import mongoSanitize from "express-mongo-sanitize"
 import { categoryRouter } from "./routes/category.route"
 import AppError from "./utils/appError"
 import errorController from "./controllers/error.controller"
@@ -10,12 +14,35 @@ import { favRouter } from "./routes/favorite.route"
 import { restaurantRouter } from "./routes/restaurant.route"
 import { orderRouter } from "./routes/order.route"
 import { dashboardRouter } from "./routes/dashboard.route"
+import path from "path"
+import compression from "compression"
+
 export const app = express()
-import "./middlewares/public.middleware"
 
 // CORS configuration
-
+app.use(cors({}))
+app.set('trust proxy', 1);
+app.set('views', path.join(__dirname, 'views'));
+app.set('views', './src.views')
+app.set('view engine', 'pug');
+app.use(
+	helmet({
+		hsts: false,
+		crossOriginResourcePolicy: false, //for images at frontend
+	})
+)
+//compress all responses
+app.use(compression())
 app.use("/api/v1/order/checkout/webhook", express.raw({ type: "*/*" }))
+const limiter = rateLimit({
+	max: 1000,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests, please try again in an hour!",
+})
+app.use("/api", limiter)
+app.use(express.json())
+app.use(mongoSanitize())
+app.use(express.static("src/public"))
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/category", categoryRouter)
 app.use("/api/v1/product", productRouter)
